d3ff85c2a199f78f24bfaa25c2b097ba6f595fb0
Usaid <46266811+usaidpeerzada@users.noreply.github.com>
2021-09-09 09:35:28 +0000

    feature/Android-SplashScreen

--- android/app/src/main/java/com/poweroutrage/MainActivity.java
+++ android/app/src/main/java/com/poweroutrage/MainActivity.java
@@ -1,6 +1,8 @@
 package com.poweroutrage;
 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +14,9 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "PowerOutrage";
   }
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this, true);
+        super.onCreate(savedInstanceState);
+    }
 }
